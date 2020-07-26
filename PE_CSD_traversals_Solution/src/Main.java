a9b9f8ee78e24dbc43f19035ecf5a14239c73bbe
Ngo Anh Tu <63779177+n-anhtu0101@users.noreply.github.com>
2020-07-26 11:58:29 +0700

    Add files via upload

---
+++ PE_CSD_traversals_Solution/src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+
+    public static void main(String[] args) {
+      int [][] b = {
+          // A B C D E F G H I J k
+            {0,1,0,1,0,0,0,0,0,0,0},//A
+            {1,0,1,0,0,0,0,0,0,0,0},//B
+            {0,1,0,1,1,0,0,0,0,0,0},//C
+            {1,0,1,0,0,1,0,0,0,0,0},//D
+            {0,0,1,0,0,0,0,1,0,0,0},//E
+            {0,0,0,1,0,0,1,0,0,0,0},//F
+            {0,0,0,0,0,1,0,0,0,0,0},//G
+            {0,0,0,0,1,0,0,0,0,0,0},//H
+            {0,0,0,0,0,0,0,0,0,1,1},//I
+            {0,0,0,0,0,0,0,0,1,0,1},//J
+            {0,0,0,0,0,0,0,0,1,1,0} //K
+      };
+      Graph g = new Graph();
+      g.setData(b);
+      System.out.println("1. Display adjacency matrix:");
+      g.dispAdj();
+      System.out.println();
+
+      System.out.println("2. Test breadth-first traversal:");
+      g.breadth(0);
+      System.out.println();
+
+      System.out.println("3. Test depth-first traversal:");
+      g.depth(0);
+      System.out.println();
+    }
+    
+}
